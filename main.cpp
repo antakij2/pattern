96585094bf548b712037fad8f3d72f3f9ee4bc81
Joe Antaki <josephantaki@brandeis.edu>
2022-07-28 19:38:51 -0400

    ignore . and .., add help, update usage string

--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
 #include <cstdlib>
 #include <cstdio>
+#include <cstring>
 #include <unistd.h>
 #include <dirent.h>
 #include "summarizer.hpp"
 
+const char* USAGE = "Usage: %s [OPTION]... DIRECTORY\n";
+
 void printUsageAndExit(char** argv)
 {
-	std::fprintf(stderr, "Usage: %s [-d delimiters] [-r] directory\n", argv[0]);
-	std::fprintf(stderr, "Or try '%s -h' for more information.\n", argv[0]); 
+	std::fprintf(stderr, USAGE, argv[0]);
+	std::fprintf(stderr, "Or try '%s -h' for more information.\n", argv[0]);
 	std::exit(EXIT_FAILURE);
 }
 
@@ -16,7 +19,7 @@ int main(int argc, char* argv[])
 {
 	int option;
 	char* delimiters = NULL;
-	while((option = getopt(argc, argv, "rd:")) != -1) 
+	while((option = getopt(argc, argv, "rd:h")) != -1)
 	{
 		switch(option) 
 		{
@@ -24,8 +27,29 @@ int main(int argc, char* argv[])
 				//TODO
 				break;
 			case 'd': 
-				delimiters = optarg; //TODO: is this safe if the arguments get permuted?
+				delimiters = optarg;
 				break;
+            case 'h':
+                std::printf(USAGE, argv[0]);
+                std::puts("");
+                std::puts("Print the pattern of the filenames in DIRECTORY.");
+                std::puts("A pattern is a sequence of columns, where each column");
+                std::puts("is a set of unique substrings of filenames.");
+                std::puts("");
+                std::puts("  -d=DELIMITERS\tdivide the pattern into columns split by the characters");
+                std::puts("\t\tin DELIMITERS; each user-perceived character (a.k.a. grapheme");
+                std::puts("\t\tcluster) in DELIMITERS is considered a separate delimiter");
+                std::puts("  -h\t\tdisplay this help text and exit");
+                std::puts("");
+                std::puts("Without a DELIMITERS argument, each character in the pattern");
+                std::puts("is placed in its own column by default.");
+                std::puts("");
+                std::puts("Example:");
+                std::printf("  %s -d abc dir\tThe pattern of the filenames in \"dir\" has a \n", argv[0]);
+                std::puts("\t\t\tnew column on each occurrence of 'a' or 'b' or 'c'.");
+
+                std::exit(EXIT_SUCCESS);
+                break;
 			default:
 				printUsageAndExit(argv);
 		}
@@ -58,10 +82,14 @@ int main(int argc, char* argv[])
 		}
 
 		dirent* ent;
+        char* filename;
 		while((ent = readdir(dir)) != NULL)
 		{
-            //TODO: ignore '.' and '..' in directory
-			summarizer.inputFilename(ent -> d_name);
+            filename = ent->d_name;
+            if(std::strcmp(".", filename) && std::strcmp("..", filename)) //ignore the "." and ".." in the directory
+            {
+                summarizer.inputFilename(filename);
+            }
 		}
 
 		closedir(dir);
